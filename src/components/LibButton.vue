8bd31ca88858af8be65807fa1a38c8dcc509a32d
dharnil mody <dharnil.armour@outlook.com>
2024-02-26 01:42:37 +1100

    Vue custom style components

---
+++ src/components/LibButton.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <button>Button UI</button>
+</template>
+
+<style scoped>
+button {
+    border-radius: 8px;
+    border: 1px solid transparent;
+    padding: 0.6em 1.2em;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    background-color: #1a1a1a;
+    cursor: pointer;
+    transition: border-color 0.25s;
+}
+
+button:hover {
+    border-color: #646cff;
+}
+
+button:active {
+    outline: 4px auto -webkit-focus-ring-color;
+}
+</style>
