39eaadfb61975555f70bd0d19cee7f5e2db068d6
dharnil mody <dharnil.armour@outlook.com>
2024-02-26 01:41:46 +1100

    vite.config.ts build and rollup plugin declared

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      formats: ['es'],
+      name: 'dharnil-component-library'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          Vue: 'vue'
+        }
+      }
+    }
+  }
 })
