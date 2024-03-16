299d15f859c796ddde8a42dfbcefcf917e7dabd9
dharnil mody <dharnil.armour@outlook.com>
2024-03-16 22:54:40 +1100

    main.ts: test bench code and export component updated

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './style.css'
 
 import LibButton from './components/LibButton.vue'
 import LibLabel from './components/LibLabel.vue'
+import MicrosoftLogon from './components/MicrosoftLogon.vue'
 
 export { LibButton, LibLabel }
 
@@ -10,12 +11,13 @@ export default {
     install: (app: App) => {
         app.component('LibButton', LibButton)
         app.component('LibLabel', LibLabel)
+        app.component('MicrosoftLogon', MicrosoftLogon)
     }
 }
 
 // Local Testing
 import { createApp } from 'vue'
-import TestApp from './App.vue'
+import Test from './Test.vue'
 
-const app = createApp( TestApp )
+const app = createApp( Test )
 app.mount( '#app' )
