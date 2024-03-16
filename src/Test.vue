0a0138264e855143c1ad5b953aad8537da5541eb
dharnil mody <dharnil.armour@outlook.com>
2024-03-16 22:53:41 +1100

    Replaced App.vue => Test.vue

---
+++ src/Test.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import MicrosoftLogon from './components/MicrosoftLogon.vue'
+
+const msalConfig = {
+    auth: {
+        clientId: "",
+        authority: "",
+        redirectUri: "http://localhost:5173/",
+    },
+    cache: {
+        cacheLocation: "",
+        storeAuthStateInCookie: false,
+    }
+}
+</script>
+
+<template>
+    <MicrosoftLogon :msal-config="msalConfig" />
+</template>
+
+<style scoped>
+</style>
