050054f5300f2d4599bf9b418c6e0485350f6748
dharnil mody <dharnil.armour@outlook.com>
2024-03-26 21:30:48 +1100

    main.ts updated with more component

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,22 @@ import './style.css'
 import LibButton from './components/LibButton.vue'
 import LibLabel from './components/LibLabel.vue'
 import MicrosoftLogon from './components/MicrosoftLogon.vue'
+import LibUnsortedIcon from './components/LibUnsortedIcon.vue';
+import LibDescIcon from './components/LibDescIcon.vue'
+import LibAscIcon from './components/LibAscIcon.vue'
+import LibTable from './components/LibTable.vue'
 
-export { LibButton, LibLabel }
+export { LibButton, LibLabel, LibUnsortedIcon, LibDescIcon, LibAscIcon, LibTable }
 
 export default {
     install: (app: App) => {
         app.component('LibButton', LibButton)
         app.component('LibLabel', LibLabel)
         app.component('MicrosoftLogon', MicrosoftLogon)
+        app.component('LibUnsortedIcon', LibUnsortedIcon)
+        app.component('LibDescIcon', LibDescIcon)
+        app.component('LibAscIcon', LibAscIcon)
+        app.component('LibTable', LibTable)
     }
 }
 
