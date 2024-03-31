dfabadfcb5a65f68cdf497e837002136400254ad
dharnil mody <dharnil.armour@outlook.com>
2024-03-31 21:49:23 +1100

    main.ts updated

--- src/main.ts
+++ src/main.ts
@@ -45,13 +45,7 @@ export default {
 
 // export default {
 //     install: (app: App) => {
-//         app.component('LibAddIcon', LibAddIcon)
-//         app.component('LibAscIcon', LibAscIcon)
 //         app.component('LibButton', LibButton)
-//         app.component('LibCancelIcon', LibCancelIcon)
-//         app.component('LibDeleteIcon', LibDeleteIcon)
-//         app.component('LibDescIcon', LibDescIcon)
-//         app.component('LibEditIcon', LibEditIcon)
 //         app.component('LibLabel', LibLabel)
 //         app.component('LibTable', LibTable)
 //         app.component('LibTextField', LibTextField)
@@ -61,10 +55,10 @@ export default {
 // }
 
 // Local Testing
-// import { createPinia } from 'pinia'
-// import { createApp } from 'vue'
-// import Test from './Test.vue'
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+import Test from './Test.vue'
 
-// const app = createApp( Test )
-// app.use(createPinia())
-// app.mount( '#app' ) 
+const app = createApp( Test )
+app.use(createPinia())
+app.mount( '#app' ) 
