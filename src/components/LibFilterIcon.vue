7ff8ad6744320bd58644450d172fab1a77256261
dharnil mody <dharnil.armour@outlook.com>
2024-04-02 14:03:06 +1100

    LibFilterIcon.vue SVG Component Added

---
+++ src/components/LibFilterIcon.vue
@@ -0,0 +1,12 @@
+<template>
+    <!-- <svg fill="#ffffff" width="100%" height="100%" viewBox="0 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink">
+        <rect width="16" height="16" id="icon-bound" fill="none" />
+        <path d="M7,16L7,10L0,2L0,0L16,0L16,2L9,10L9,14L7,16Z" />
+    </svg> -->
+    <svg fill="#ffffff" width="100%" height="100%" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+        <rect x="16" y="120" width="480" height="54" />
+        <rect x="96" y="232" width="320" height="54" />
+        <rect x="192" y="344" width="128" height="54" />
+    </svg>
+</template>
